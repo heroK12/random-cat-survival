dd7d3638abdcedc997e455853b863d5f92aa4605
heroK12 <koudai2517@gmail.com>
2023-03-27 18:10:53 +0900

    ♻️ useCallbackを使用するように変更

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,14 +1,18 @@
+import { memo } from "react";
 import styles from "../pages/index.module.css";
 
 type Props = {
     handleClick: () => void;
 };
 
-export const Button = (props: Props) => {
+export const Button = memo((props: Props) => {
     const { handleClick } = props;
     return (
-        <button onClick={handleClick} className={styles.button}>
-            他のにゃんこも見る
+        <button
+            onClick={handleClick}
+            className={styles.button}
+        >
+            きょうのにゃんこ🐱
         </button>
     );
-};
+});
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,8 @@
 import { GetServerSideProps, NextPage } from "next";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { fetchImage } from "../model/Image";
 import { Button } from "../components/Button";
 import styles from "./index.module.css";
-
 
 type Props = {
     initialImageUrl: string;
@@ -11,27 +10,17 @@ type Props = {
 
 const indexPage: NextPage<Props> = ({ initialImageUrl }) => {
     const [imageUrl, setImageUrl] = useState(initialImageUrl);
-    const [loading, setLoading] = useState(false);
 
-    // useEffect(() => {
-    //     fetchImage().then((newImage) => {
-    //         setImageUrl(newImage.url);
-    //         setLoading(false);
-    //     });
-    // }, []);
-
-    const handleClick = async () => {
-        setLoading(true);
+    const handleClick = useCallback(async () => {
         const newImage = await fetchImage();
         setImageUrl(newImage.url);
-        setLoading(false);
-    };
+    }, []);
 
     return (
         <div className={styles.page}>
             <Button handleClick={handleClick} />
             <div className={styles.frame}>
-                {loading || <img src={imageUrl} className={styles.img} />}
+                <img src={imageUrl} className={styles.img} />
             </div>
         </div>
     );
